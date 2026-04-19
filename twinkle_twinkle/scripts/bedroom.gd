4eb9709525098a18efdf6cfc31af9ae9ce9c9625
kaytrxg <trogdonkay@gmail.com>
2026-04-19 00:19:11 -0400

    Updated GCloud with Twinkle Twinkle

--- twinkle_twinkle/scripts/bedroom.gd
+++ twinkle_twinkle/scripts/bedroom.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+@onready var http_request: HTTPRequest = $HTTPRequest
+ 
+func _log_play() -> void:
+	var url := "https://godot-play-api-831129541610.us-east1.run.app/log/twinkle-twinkle"
+	var headers = ["Content-Type: application/json", "Content-Length: 2"]
+	#http_request.request_completed.connect(_on_request_completed)
+	var error = http_request.request(url, headers, HTTPClient.METHOD_POST, "{}")
+	if error != OK:
+		print("HTTPRequest failed: ", error)
+	
+
 func _ready() -> void:
 	# Character and button will be added separately.
+	_log_play()
 	pass
