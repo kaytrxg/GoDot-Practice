f348d3347894758e1e929b15100ca58ed264ab46
kaytrxg <trogdonkay@gmail.com>
2026-05-31 22:04:19 -0400

    Added two space related scenes, quick skeleton before review

---
+++ twinkle_twinkle/scripts/space_float.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready() -> void:
+	# Character floating and necklace interaction will be added separately.
+	pass
---
+++ twinkle_twinkle/scripts/space_transition.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready() -> void:
+	# Character rising animation will be added separately.
+	pass
